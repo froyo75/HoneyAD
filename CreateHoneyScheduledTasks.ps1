edf147467ef731581ad8219d2279efedc80cba0f
froyo75 <froyo75@users.noreply.github.com>
2022-02-23 11:56:46 +0100

    Fix scheduled task 'Action' parameter

--- CreateHoneyScheduledTasks.ps1
+++ CreateHoneyScheduledTasks.ps1
@@ -4,15 +4,18 @@
 .DESCRIPTION
     Create scheduled tasks automatically for the Honeypot project
 .NOTES
-  Version:        1.1
+  Version:        1.2
   Author:         @froyo75
   Creation Date:  08/09/2020
   Purpose/Change: 
         - 1.0 Initial script development
         - 1.1 Logging improvements
+        - 1.2 Fix scheduled task 'Action' parameter
 #>
 
 $LogFile = "$PSScriptRoot\$(($MyInvocation.Mycommand.Name).replace("ps1", "log"))"
+$WriteToLog = $true
+$DisplayMessage = $true
 
 function Get-DateTime() {
     return $(Get-Date -UFormat '%m/%d/%Y %T')
@@ -41,9 +44,11 @@ function Create-NewScheduledTask([string]$TaskName, [string]$Description, [strin
         $ScheduledTaskObject = (Get-ScheduledTask -TaskName $TaskName)
         if ($ScheduledTaskObject -ne $null) {
             Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false
+            $Message = "[-] $(Get-DateTime): Removing the scheduled task '$TaskName\$Description'"
+            Logging -Message $Message -Color "Magenta" -LogFile $Logfile -WriteToLog $WriteToLog -DisplayMessage $DisplayMessage
         }
         $NewScheduledTaskAction = New-ScheduledTaskAction -Execute 'Powershell.exe' `
-        -Argument '-NoProfile -WindowStyle Hidden -command ". C:\Users\Administrator\Desktop\RS\SimulateConnections.ps1;SimulateConnections -CSVFile C:\Users\Administrator\Desktop\RS\test.csv"'
+        -Argument "-NoProfile -WindowStyle Hidden -command `"$Action`""
         $NewScheduledTaskTrigger = iex "New-ScheduledTaskTrigger $TriggerArgs"
         $Message = "[+] $(Get-DateTime): Creating a new scheduled task '$TaskName\$Description'"
         Logging -Message $Message -Color "green" -LogFile $Logfile -WriteToLog $WriteToLog -DisplayMessage $DisplayMessage
@@ -58,30 +63,30 @@ function Create-NewScheduledTask([string]$TaskName, [string]$Description, [strin
     } 
 }
 
-#Create a new scheduled task for updating HoneyUsers' password automatically
-$TaskName = "Update HoneyUsers password"
-$Description = "Update the HoneyUsers' password automatically "
+#Create a new scheduled task for updating HoneyUsersServices' password automatically
+$TaskName = "Update HoneyUsersServices password"
+$Description = "Update the HoneyUsersServices' password automatically "
 $Action = ". C:\Users\Administrator\Desktop\RS\UpdateHoneyUsersOrComputersPwd.ps1;UpdateHoneyUsersOrComputersPwd -CSVFile C:\Users\Administrator\Desktop\RS\UsersServices.csv -ObjectType user -PassLength 40 -Delay 5"
 $Trigger =  "-Daily -At 9am"
 Create-NewScheduledTask -TaskName $TaskName -Description $Description -Action $Action -TriggerArgs $Trigger
 
 #Create a new scheduled task for updating HoneyComputers' password automatically
-$TaskName = "Update HoneyUsers password"
-$Description = "Update the HoneyUsers' password automatically "
+$TaskName = "Update HoneyComputers password"
+$Description = "Update the HoneyComputers' password automatically "
 $Action = ". C:\Users\Administrator\Desktop\RS\UpdateHoneyUsersOrComputersPwd.ps1;UpdateHoneyUsersOrComputersPwd -CSVFile C:\Users\Administrator\Desktop\RS\Computers.csv -ObjectType computer -PassLength 40 -Delay 5"
 $Trigger =  "-Daily -At 5am"
 Create-NewScheduledTask -TaskName $TaskName -Description $Description -Action $Action -TriggerArgs $Trigger
 
-#Create a new scheduled task for simulating successful or bad network logon attempts using the HoneyUsers
-$TaskName = "Simulate HoneyUsers Connections"
-$Description = "Simulate successful or bad network logon attempts using the HoneyUsers"
+#Create a new scheduled task for simulating successful or bad network logon attempts using the HoneyUsersServices
+$TaskName = "Simulate HoneyUsersServices Connections"
+$Description = "Simulate successful or bad network logon attempts using the HoneyUsersServices"
 $Action = ". C:\Users\Administrator\Desktop\RS\SimulateHoneyConnections.ps1;SimulateHoneyConnections -CSVFile C:\Users\Administrator\Desktop\UsersServices.csv -ObjectType user -MaxBadPwdCount 2"
 $Trigger =  "-Once -At (Get-Date).Date -RepetitionInterval (New-TimeSpan -Hours 2)"
 Create-NewScheduledTask -TaskName $TaskName -Description $Description -Action $Action -TriggerArgs $Trigger
 
 #Create a new scheduled task for simulating successful or bad network logon attempts using the HoneyComputers
-$TaskName = "Simulate HoneyUsers Connections"
-$Description = "Simulate successful or bad network logon attempts using the HoneyUsers"
+$TaskName = "Simulate HoneyComputers Connections"
+$Description = "Simulate successful or bad network logon attempts using the HoneyComputers"
 $Action = ". C:\Users\Administrator\Desktop\RS\SimulateHoneyConnections.ps1;SimulateHoneyConnections -CSVFile C:\Users\Administrator\Desktop\RS\Computers.csv -ObjectType computer -MaxBadPwdCount 2"
 $Trigger =  "-Once -At (Get-Date).Date -RepetitionInterval (New-TimeSpan -Hours 24)"
 Create-NewScheduledTask -TaskName $TaskName -Description $Description -Action $Action -TriggerArgs $Trigger
